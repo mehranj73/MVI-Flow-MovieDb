4a070f7038d9387a37735003b25acd0c64bb4f88
Mehran Jalili <mehranj73@gmail.com>
2020-09-12 15:57:56 +0430

    create models for movie and movie detail

---
+++ app/src/main/java/com/mehranj73/moviedb/data/model/Genre.kt
@@ -0,0 +1,6 @@
+package com.mehranj73.moviedb.data.model
+
+data class Genre(
+    val id: Int,
+    val name: String
+)
---
+++ app/src/main/java/com/mehranj73/moviedb/data/model/MovieResponse.kt
@@ -0,0 +1,8 @@
+package com.mehranj73.moviedb.data.model
+
+data class MovieResponse(
+    val page: Int,
+    val results: List<Movie>,
+    val total_pages: Int,
+    val total_results: Int
+)
