6c155e88fe60870c6acb6f79baab53e10fc49c47
Mehran Jalili <mehranj73@gmail.com>
2020-09-15 21:08:12 +0430

    create movies adapter and poster base urls' extension functions

---
+++ app/src/main/java/com/mehranj73/moviedb/ui/movie/MovieAdapter.kt
@@ -0,0 +1,95 @@
+package com.mehranj73.moviedb.ui.movie
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.RequestManager
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions.withCrossFade
+import com.mehranj73.moviedb.R
+import com.mehranj73.moviedb.data.model.Movie
+import com.mehranj73.moviedb.util.w500PosterUrl
+
+class NewsAdapter(
+    private val requestManager: RequestManager
+) : RecyclerView.Adapter<NewsAdapter.MovieViewHolder>() {
+
+
+    inner class MovieViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+        private val title: TextView = itemView.findViewById(R.id.title_textView)
+        private val releaseDate: TextView = itemView.findViewById(R.id.release_date_textView)
+        private val voteAverage: TextView = itemView.findViewById(R.id.vote_average_textView)
+        private val overview: TextView = itemView.findViewById(R.id.overview_textView)
+        private var poster: ImageView = itemView.findViewById(R.id.poster_imageView)
+
+        fun bind(movie: Movie){
+            title.text = movie.title
+            releaseDate.text = movie.release_date
+            voteAverage.text = movie.vote_average.toString()
+            overview.text = movie.overview
+
+            requestManager
+                .load(movie.poster_path.w500PosterUrl())
+                .transition(withCrossFade())
+                .into(poster)
+
+
+            itemView.setOnClickListener {
+                onItemClickListener?.let {
+                    it(movie)
+                }
+            }
+
+
+        }
+
+    }
+
+    private val differCallback = object : DiffUtil.ItemCallback<Movie>(){
+
+        override fun areItemsTheSame(oldItem: Movie, newItem: Movie): Boolean {
+            return oldItem.id == newItem.id
+
+        }
+
+        override fun areContentsTheSame(oldItem: Movie, newItem: Movie): Boolean {
+            return oldItem == newItem
+        }
+
+
+    }
+
+    val differ = AsyncListDiffer(this, differCallback)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieViewHolder {
+        return MovieViewHolder(
+            LayoutInflater.from(parent.context).inflate(
+                R.layout.movie_item,
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun getItemCount(): Int {
+        return differ.currentList.size
+    }
+
+    override fun onBindViewHolder(holder: MovieViewHolder, position: Int) {
+        val movie = differ.currentList[position]
+        holder.bind(movie)
+
+    }
+
+    private var onItemClickListener: ((Movie) -> Unit)? = null
+
+
+    fun setOnItemClickListener(listener: (Movie) -> Unit) {
+        onItemClickListener = listener
+    }
+
+}
---
+++ app/src/main/java/com/mehranj73/moviedb/util/BaseImageUrlExt.kt
@@ -0,0 +1,37 @@
+package com.mehranj73.moviedb.util
+
+
+
+fun String.w92PosterUrl(): String{
+    return "https://image.tmdb.org/t/p/w92${this}"
+
+}
+
+fun String.w154PosterUrl(): String{
+    return "https://image.tmdb.org/t/p/w154${this}"
+
+}
+
+fun String.w185PosterUrl(): String{
+    return "https://image.tmdb.org/t/p/w185${this}"
+
+}
+
+fun String.w342PosterUrl(): String{
+    return "https://image.tmdb.org/t/p/w342${this}"
+
+}
+
+fun String.w500PosterUrl(): String{
+    return "https://image.tmdb.org/t/p/w500${this}"
+
+}
+
+fun String.w780PosterUrl(): String{
+    return "https://image.tmdb.org/t/p/w7802${this}"
+
+}
+fun String.originalPosterUrl(): String{
+    return "https://image.tmdb.org/t/p/original${this}"
+
+}
