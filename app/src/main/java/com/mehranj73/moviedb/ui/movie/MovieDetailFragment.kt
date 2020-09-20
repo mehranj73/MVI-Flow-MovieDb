b23451957c39b46b272c567b4ba86b7f67555d98
Mehran Jalili <mehranj73@gmail.com>
2020-09-20 23:43:42 +0330

    setup movie detail fragment and make it working. bug: too many dialogs

--- app/src/main/java/com/mehranj73/moviedb/ui/movie/MovieDetailFragment.kt
+++ app/src/main/java/com/mehranj73/moviedb/ui/movie/MovieDetailFragment.kt
@@ -4,21 +4,33 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import androidx.lifecycle.Observer
+import com.bumptech.glide.RequestManager
 import com.mehranj73.moviedb.R
 import com.mehranj73.moviedb.data.model.Movie
 import com.mehranj73.moviedb.ui.movie.state.MovieStateEvent.MovieDetailEvent
 import com.mehranj73.moviedb.util.StateMessageCallback
+import com.mehranj73.moviedb.util.originalPosterUrl
+import com.mehranj73.moviedb.util.w154PosterUrl
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.movie_detail_fragment.*
+import kotlinx.android.synthetic.main.movie_detail_fragment.overview_textView
+import kotlinx.android.synthetic.main.movie_detail_fragment.poster_imageView
+import kotlinx.android.synthetic.main.movie_detail_fragment.title_textView
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.FlowPreview
+import javax.inject.Inject
 
 private const val TAG = "MovieDetailFragment"
 
 @ExperimentalCoroutinesApi
 @FlowPreview
+@AndroidEntryPoint
 class MovieDetailFragment(
 
 ) : BaseMovieFragment(R.layout.movie_detail_fragment) {
 
+    @Inject
+    lateinit var requestManager: RequestManager
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -35,7 +47,7 @@ class MovieDetailFragment(
         viewModel.viewState.observe(viewLifecycleOwner, Observer { viewState ->
             viewState.movieDetailFields.let { movieDetailFields ->
                 movieDetailFields.movie?.let {
-                    Log.d(TAG, "subscribeObservers: ${it.title}")
+                    setMovieDetail(it)
                 }
 
 
@@ -61,6 +73,34 @@ class MovieDetailFragment(
 
             })
         })
+
+    }
+
+    private fun setMovieDetail(movie: Movie){
+        title_textView.text = movie.title
+        score_textView.text = movie.vote_average.toString()
+        overview_textView.text = movie.overview
+       // release_date_textView.text = movie.release_date
+
+        requestManager
+            .load(movie.poster_path.originalPosterUrl())
+            .thumbnail(requestManager.load(movie.poster_path.w154PosterUrl()))
+            .into(poster_imageView)
+
+        movie.revenue?.let {
+            revenu_textView.text = it.toString()
+        }
+        movie.budget?.let {
+            budget_textView.text = it.toString()
+        }
+        movie.status?.let {
+            revenu_textView.text = it
+        }
+        movie.runtime?.let {
+            runtime_textView.text = it.toString()
+        }
+
+
 
 
     }
